a7b9c22f61d3c9017d51bfff204c9a1e194fd746
mohamedyoussefgaber <74613904+mohamedyoussefgaber@users.noreply.github.com>
2021-01-30 16:54:37 +0000

    Update add_maker.sh

--- New3/src/add_maker.sh
+++ New3/src/add_maker.sh
@@ -5,4 +5,4 @@ xterm -e "roslaunch my_robot world.launch"&
 sleep 3
 xterm -e "roslaunch my_robot amcl.launch" &
 sleep 3
-xterm -e "rosrun add_makers add_makers2"
+xterm -e "rosrun add_makers add_makers"
